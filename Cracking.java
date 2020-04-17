5afd6767c66655b224f988e429fb9a2724dc9239
yardenSchwartz <49039527+yardenSchwartz@users.noreply.github.com>
2020-04-17 14:10:43 +0300

    Delete Cracking.java

--- Cracking.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public interface Cracking {
-
-    void crack(byte[]cipherText, byte[]plainText, File output);
-    void shiftColumns(byte[]array);
-    void roundKey(byte[]array, byte[]key);
-
-}
