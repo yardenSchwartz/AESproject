deb89fb00c87c1d34c594f327039841e7092a061
yardenSchwartz <49039527+yardenSchwartz@users.noreply.github.com>
2020-04-17 14:11:13 +0300

    Delete EncryptionOrDecryption.java

--- EncryptionOrDecryption.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public interface EncryptionOrDecryption {
-
-    void EncOrDec(byte[]inputFile, File outputFile, byte[]keys);
-    void shiftColumns(byte[]array);
-    void roundKey(byte[]array, byte[]key);
-
-}
